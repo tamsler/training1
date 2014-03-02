f41250c557f921db577f1d817aa11c59116711a2
Thomas Amsler <tamsler@gmail.com>
2014-03-02 13:45:38 -0800

    Working on routing.

--- app/js/app.js
+++ app/js/app.js
@@ -6,20 +6,20 @@
     /*
      * Creating training module
      */
-    angular.module('training', ['training.controllers', 'training.services','training.directives','ngRoute'])
-        /*
-         * Set up some routes.
-         */
-        .config(function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    controller:'MainCtrl',
-                    templateUrl:'partials/main-partial.html'
-                })
-                .otherwise({
-                    redirectTo:'/'
-                });
-        });
+
+    angular.module('training', ['ngRoute', 'training.controllers', 'training.services','training.directives']);
+
+    angular.module('training').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/users', {controller:'MainCtrl', templateUrl:'partials/main-partial.html'})
+            .when('/users/edit', {controller:'MainCtrl', templateUrl:'partials/user-edit.html'})
+            .otherwise({
+                redirectTo:'/'
+            });
+
+        //$locationProvider.html5Mode(true).hashPrefix('!');
+    }]);
 
     /*
      * Create training controllers module
